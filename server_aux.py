192ae0b663b77e5f973e8d8ab71c411cb988c75d
vicenteLV <vicentelopez@ug.uchile.cl>
2026-08-19 13:52:45 -0400

    parse and create

---
+++ server_aux.py
@@ -0,0 +1,43 @@
+
+def parse_HTTP_message(http_message: bytes) -> dict:
+    """bytes -> dict
+    receives http message on bytes format and extracts HEAD (startline + headers) + BODY"""
+    http_dict = {}
+    http_plain = http_message.decode()
+
+    head_body = http_plain.split("\r\n\r\n") #body and head separation on a linked list
+    head_split = head_body[0].split("\r\n")  #startline + headers
+
+    http_dict["startline"] = head_split[0]   #string
+    http_dict["headers"] = head_split[1:]    #list(str)
+    http_dict["BODY"] = head_body[1]         #string
+
+    return http_dict
+
+
+def create_HTTP_message(http_struct: dict) -> bytes:
+    """dict -> bytes
+    takes a dict with message info and builds the http and returns it encoded"""
+    http_msg = ""
+
+    http_msg += f"{http_struct["startline"]}\r\n"
+
+    for i in range(len(http_struct["headers"])):
+        http_msg += f"{http_struct["headers"][i]}\r\n"
+
+    http_msg += "\r\n" #double \r\n after final header
+
+    http_msg += http_struct["BODY"]
+
+    return http_msg.encode()
+
+
+
+
+
+
+
+
+
+
+
