17d17cedda6d2935a71f99ce147ede8f15b343e2
vicenteLV <vicentelopez@ug.uchile.cl>
2026-08-22 21:39:56 -0400

    proxy server v1

--- server.py
+++ server.py
@@ -1,1 +1,32 @@
+import socket
+from server_aux import *
+
+BUFSIZE = 1024
+END_OF_MSG = "\n"
+
+server_ad = ("192.168.100.137", 8000)
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(server_ad)
+
+server_socket.listen(3)
+print("Esperando clientes ...")
+
+while True:
+    client_socket, client_ad = server_socket.accept()
+
+    client_msg = server_socket.recv(BUFSIZE)
+
+    print(f"Mensaje recibido: {client_msg.decode()}")
+
+    msg_respuesta = "Mensaje recibido" + END_OF_MSG
+    client_socket.send(msg_respuesta.encode())
+
+    client_socket.close()
+
+    print(f"Conexión con {client_ad} ha sido cerrada")
+
+
+
+
 
