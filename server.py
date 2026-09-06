3b2324c3f4746d1069bad0587ffa6c56806ca72f
vicenteLV <vicentelopez@ug.uchile.cl>
2026-09-06 12:58:40 -0300

    server v1

--- server.py
+++ server.py
@@ -4,7 +4,9 @@ from server_aux import *
 BUFSIZE = 1024
 END_OF_MSG = "\n"
 
-server_ad = ("192.168.100.137", 8000)
+server_address = "192.168.100.137"
+
+server_ad = (server_address, 8000)
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(server_ad)
@@ -15,9 +17,9 @@ print("Esperando clientes ...")
 while True:
     client_socket, client_ad = server_socket.accept()
 
-    client_msg = server_socket.recv(BUFSIZE)
+    client_msg = client_socket.recv(BUFSIZE)
 
-    print(f"Mensaje recibido: {client_msg.decode()}")
+    print(f"Mensaje recibido:\n{client_msg.decode()}")
 
     msg_respuesta = "Mensaje recibido" + END_OF_MSG
     client_socket.send(msg_respuesta.encode())
