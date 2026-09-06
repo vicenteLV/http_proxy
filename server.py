a504f5f753d942000d754529efd6a1b6fe4528ae
vicenteLV <vicentelopez@ug.uchile.cl>
2026-09-06 17:21:55 -0300

    date and server logic

--- server.py
+++ server.py
@@ -1,10 +1,16 @@
 import socket
 from server_aux import *
 
+#CTTES
 BUFSIZE = 1024
 END_OF_MSG = "\n"
 
-server_address = "0.0.0.0" #ip maquina virtual
+#dict for code answer pairs
+cod_ans = {
+    "200": "OK"
+}
+
+server_address = "0.0.0.0" 
 
 server_ad = (server_address, 8000)
 
@@ -15,14 +21,51 @@ server_socket.listen(3)
 print("Esperando clientes ...")
 
 while True:
+    response = ""
+
     client_socket, client_ad = server_socket.accept()
 
     client_msg = client_socket.recv(BUFSIZE)
 
-    print(f"Mensaje recibido:\n{client_msg}")
+    #print(f"Mensaje recibido:\n{client_msg}")
 
-    msg_respuesta = "Mensaje recibido" + END_OF_MSG
-    client_socket.send(msg_respuesta.encode())
+    #extract info from dict
+    http_request = parse_HTTP_message(client_msg.decode())
+    startline_list = http_request["startline"].split(" ")
+    q_method = startline_list[0]
+    q_route = startline_list[1]
+    q_version = startline_list[2]
+    print(f"Method: {q_method}, route: {q_route}, version http: {q_version}")
+
+    if q_method == "GET" and q_route == "/":
+
+        with open("./content/response.html") as file:
+            cont = file.read()
+
+        cont_bytes = cont.encode()
+        len_bodyResponse = len(cont_bytes)
+
+        #startline
+        code = "200"
+        code_msg = cod_ans[code]
+        response_startline = q_version + code + code_msg + "\r\n"
+        response += response_startline
+
+        #head
+        response += "Server: http_proxy_redes\r\n"   #Server
+        response += "Date: " + obtain_date() + "\r\n"
+        response += "Content-Type: text/html; charset=utf-8\r\n"
+        response += "Content-Length: " + str(len_bodyResponse) + "\r\n"
+        response += "Connection: keep-alive\r\n"
+
+        reponse += "\r\n"
+
+        #Body
+        response += cont
+
+        client_socket.send(response.encode())
+
+        
 
     client_socket.close()
 
--- server_aux.py
+++ server_aux.py
@@ -1,3 +1,7 @@
+import socket
+
+BUFSIZE = 1024
+
 
 def parse_HTTP_message(http_message: bytes) -> dict:
     """bytes -> dict
@@ -9,7 +13,15 @@ def parse_HTTP_message(http_message: bytes) -> dict:
     head_split = head_body[0].split("\r\n")  #startline + headers
 
     http_dict["startline"] = head_split[0]   #string
-    http_dict["headers"] = head_split[1:]    #list(str)
+
+    http_dict["HEAD"] = {}                   #dict for headers
+    head_len = len(head_split) - 1  
+       
+    for i in range(1, head_len):
+        header_split = head_split[i].split(": ")
+        http_dict["HEAD"][header_split[0]] = header_split[1]
+
+
     http_dict["BODY"] = head_body[1]         #string
 
     return http_dict
@@ -30,6 +42,31 @@ def create_HTTP_message(http_struct: dict) -> bytes:
     http_msg += http_struct["BODY"]
 
     return http_msg.encode()
+
+
+def obtain_date(url: str = "cc4303.bachmann.cl") -> str:
+    """str -> str
+    method for obtaining current date extracting it from an http response from a domain 
+    default url: cc4303.bachmann.cl"""
+
+    date_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    date_socket.connect((url, 80))
+
+    msg = "GET / HTTP/1.1\r\nHost: cc4303.bachmann.cl\r\nUser-Agent: curl/8.5.0\r\nAccept: */*\r\n\r\n"
+    date_socket.send(msg.encode())
+
+    received = date_socket.recv(BUFSIZE)
+    received_decoded = parse_HTTP_message(received)
+
+    date_socket.close()
+
+    return received_decoded["HEAD"]["Date"]
+
+
+if __name__ == "__main__":
+    print(obtain_date())
+
+    
 
 
 
